a53a17884341af748277bddd6987a00b53cd624c
Aastha <aasthathapaliya@gmail.com>
2023-11-10 10:45:44 +0100

    Added unit test cases

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -12,19 +12,37 @@ class TestMathGame(unittest.TestCase):
             rand_num = generate_random_number(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def generate_random_operation(self):
+        # Test if random operator generated are within the specified list
+        for _ in range(1000): # Test a large number of random operations
+            operation = generate_random_operation()
+            self.assertIn(operation, {'+', '-', '*'}, f"Unexpected operation: {operation}")
 
-    def test_function_C(self):
+
+    def perform_operation(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (5, 2, '+', '5 + 2', 7), # Addition
+                (0, 0, '+', '0 + 0', 0),
+                (-3, -8, '+', '-3 + 8', 5),
+                (2.5, 3.5, '+', '2.5 + 3.5', 6),
+
+                (5, 2, '-', '5 - 2', 3), # Subtraction
+                (0, 0, '-', '0 - 0', 0),
+                (-3, 8, '-', '-3 - 8', -11),
+                (2.5, 3.5, '-', '2.5 - 3.5', -1),
+
+                (5, 2, '*', '5 * 2', 10), # Multiplication
+                (0, 0, '*', '0 * 0', 0),
+                (-3, 8, '*', '-3 * 8', -24),
+                (2.5, 3.5, '*', '2.5 * 3.5', 8.75),
             ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+            for number_1, number_2, operator, expected_problem, expected_answer in test_cases:
+                with self.subTest(number_1=number_1, number_2=number_2, operator=operator):
+                    problem, result = perform_operation(number_1, number_2, operator)
+                    self.assertEqual(problem, expected_problem)
+                    self.assertEqual(result, expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
