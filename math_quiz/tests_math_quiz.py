1590830eae07f94b8b94eb064fb8fb066c590b74
Aastha <aasthathapaliya@gmail.com>
2023-11-10 16:19:49 +0100

    fixed dependencies issue

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -14,34 +14,33 @@ class TestMathGame(unittest.TestCase):
 
     def generate_random_operation(self):
         # Test if random operator generated are within the specified list
-        for _ in range(1000): # Test a large number of random operations
+        for _ in range(1000):  # Test a large number of random operations
             operation = generate_random_operation()
             self.assertIn(operation, {'+', '-', '*'}, f"Unexpected operation: {operation}")
 
-
     def perform_operation(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7), # Addition
-                (0, 0, '+', '0 + 0', 0),
-                (-3, -8, '+', '-3 + 8', 5),
-                (2.5, 3.5, '+', '2.5 + 3.5', 6),
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),  # Addition
+            (0, 0, '+', '0 + 0', 0),
+            (-3, -8, '+', '-3 + 8', 5),
+            (2.5, 3.5, '+', '2.5 + 3.5', 6),
 
-                (5, 2, '-', '5 - 2', 3), # Subtraction
-                (0, 0, '-', '0 - 0', 0),
-                (-3, 8, '-', '-3 - 8', -11),
-                (2.5, 3.5, '-', '2.5 - 3.5', -1),
+            (5, 2, '-', '5 - 2', 3),  # Subtraction
+            (0, 0, '-', '0 - 0', 0),
+            (-3, 8, '-', '-3 - 8', -11),
+            (2.5, 3.5, '-', '2.5 - 3.5', -1),
 
-                (5, 2, '*', '5 * 2', 10), # Multiplication
-                (0, 0, '*', '0 * 0', 0),
-                (-3, 8, '*', '-3 * 8', -24),
-                (2.5, 3.5, '*', '2.5 * 3.5', 8.75),
-            ]
+            (5, 2, '*', '5 * 2', 10),  # Multiplication
+            (0, 0, '*', '0 * 0', 0),
+            (-3, 8, '*', '-3 * 8', -24),
+            (2.5, 3.5, '*', '2.5 * 3.5', 8.75),
+        ]
 
-            for number_1, number_2, operator, expected_problem, expected_answer in test_cases:
-                with self.subTest(number_1=number_1, number_2=number_2, operator=operator):
-                    problem, result = perform_operation(number_1, number_2, operator)
-                    self.assertEqual(problem, expected_problem)
-                    self.assertEqual(result, expected_answer)
+        for number_1, number_2, operator, expected_problem, expected_answer in test_cases:
+            with self.subTest(number_1=number_1, number_2=number_2, operator=operator):
+                problem, result = perform_operation(number_1, number_2, operator)
+                self.assertEqual(problem, expected_problem)
+                self.assertEqual(result, expected_answer)
 
 
 if __name__ == "__main__":
